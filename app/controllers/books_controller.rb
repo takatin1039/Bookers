0749acd6b72d1bf5e388e0a392341eab0a811ee8
takatin1039 <kinoshita.takayuki.infratop@gmail.com>
2020-03-24 10:49:51 +0900

    Initial Commit Message

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,49 @@
+class BooksController < ApplicationController
+  def index
+  	#記事を全権取得
+  	@books = Book.all
+    @book = Book.new
+   
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+    book = Book.new(book_params)
+        if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+
+
+    end
+  end
+  def edit
+  	@book = Book.find(params[:id])
+  end
+  def update
+    @book = Book.find(params[:id])
+  	book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice2] = "Book was successfully updated"
+        redirect_to book_path(@book.id)
+    else render action: :edit
+  	
+    end
+  end
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+    
+  end
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  	
+  end
+end
