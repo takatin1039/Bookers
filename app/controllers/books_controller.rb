e8c21aa0805b6375efc865e4e90e3b89a6d2dd5d
takatin1039 <kinoshita.takayuki.infratop@gmail.com>
2020-11-15 02:03:27 +0900

    a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,7 +12,6 @@ class BooksController < ApplicationController
   def create
     @books = Book.all
     @book = Book.new(book_params)
-    book = Book.new(book_params)
         if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
